f61ce968376a47d1c98c20eadcc40dfb053f07ab
Shahzod2727 <shahzodjava@gmail.com>
2022-04-12 10:21:49 +0500

    part 331

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MeetupList from "../components/meetups/MeetupList";
+// import Layout from "../components/layout/Layout";
 
 export default function HomePage() {
   const DUMMY_MEETUPS = [
