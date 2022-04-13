04da0be6e7a992e8458a90cfa2d1cbd4ec671ece
Shahzod2727 <shahzodjava@gmail.com>
2022-04-13 13:29:02 +0500

    part 341

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,36 @@
 import React, { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
-// import Layout from "../components/layout/Layout";
+import { MongoClient } from "mongodb";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Thomas_Wolfe%27s_Home.jpg/1280px-Thomas_Wolfe%27s_Home.jpg",
-    address: "Some address 5, 12345 SomeCity",
-    description: "some data",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Thomas_Wolfe%27s_Home.jpg/1280px-Thomas_Wolfe%27s_Home.jpg",
-    address: "Some address 5, 12345 SomeCity",
-    description: "some data",
-  },
-];
 
 export default function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
+
+
+
 export async function getStaticProps(props) {
-  // const res = await fetch('https://.../posts')
-  // const posts = await res.json()
+  const client = await MongoClient.connect(
+    "mongodb+srv://shaxzod:shaxzod27@cluster0.1bcrt.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
 
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
-    revalidate: 10,
+    revalidate: 1,
   };
 }
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,40 @@
-// our-getDomainLocale.com/new-meetup
-import React from "react";
-import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+// // our-getDomainLocale.com/new-meetup
+// import React from "react";
+// import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
-export default function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData) {
-    console.log(enteredMeetupData)
+// export default function NewMeetupPage() {
+//   function addMeetupHandler(enteredMeetupData) {
+//     console.log(enteredMeetupData)
+//   }
+//   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+// }
+
+
+// our-domain.com/new-meetup
+import { useRouter } from 'next/router';
+
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+function NewMeetupPage() {
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push('/');
   }
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />
 }
+
+export default NewMeetupPage;
